fa8374d61f38b67a1aad9907c8dfcd1ccfae4158
cokovskak <cokovskak@gmail.com>
2024-07-13 15:46:02 +0200

    Last requested changes implented

--- homework2/reviewapp/src/components/features/shows/ShowDetails/ShowDetails.tsx
+++ homework2/reviewapp/src/components/features/shows/ShowDetails/ShowDetails.tsx
@@ -11,7 +11,7 @@ export const ShowDetails=({show}: IShowItemProps) =>
     return (
         
             <Card marginBottom={5}>
-               <Image   alt=" " src={ show.imageUrl ? show.imageUrl: 'https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/bk99/brooklyn-99-social.jpg/_jcr_content/renditions/original.JPEG'}/>
+               <Image   alt={show.title} src={ show.imageUrl ? show.imageUrl: 'https://roost.nbcuni.com/bin/viewasset.html/content/dam/Peacock/Landing-Pages/2-0-design/bk99/brooklyn-99-social.jpg/_jcr_content/renditions/original.JPEG'}/>
                 <CardBody>
                     <Heading size="md" padding={3}>{show.title}</Heading>
                     <Text padding={3}>{show.description}</Text>
