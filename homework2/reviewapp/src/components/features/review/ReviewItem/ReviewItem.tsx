bf56349aed34ea382410768d36bcad00016608f4
cokovskak <cokovskak@gmail.com>
2024-07-13 15:43:55 +0200

    Last requested changes implented

--- homework2/reviewapp/src/components/features/review/ReviewItem/ReviewItem.tsx
+++ homework2/reviewapp/src/components/features/review/ReviewItem/ReviewItem.tsx
@@ -1,3 +1,4 @@
+import StarsRatingInput from "@/components/shared/StarsRating/StarsRatingInput";
 import { IReview } from "@/typings/review";
 import { StarIcon } from "@chakra-ui/icons";
 import { Button, Flex,Text } from "@chakra-ui/react";
@@ -13,11 +14,7 @@ export const ReviewItem=({review,onDelete}:IReviewItemProps)=>{
         <Flex bg="#371587" fontSize='xl' borderRadius="15px" flexDirection="column" marginBottom={3} width= "100%" padding={8} height="fit-content" textColor="white" border="15px" align="flex-start" >
             <Text marginBottom={4}>{review.comment}</Text>
             <Text marginBottom={4}>{`${review.rating}/5`}</Text>
-            <Flex marginBottom={4}>
-          {Array(5).fill('').map((_, index) => (
-              <StarIcon
-                key={index} boxSize={5}  color={index < review.rating ? 'yellow.400' : 'gray.300'}/> ))}
-        </Flex>
+            <StarsRatingInput value={review.rating}/>
             <Button   cursor="pointer" onClick={()=>onDelete(review)}>
                 Remove
             </Button>
